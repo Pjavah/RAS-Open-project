270c7d1a436e571bc3f9c46fdb4a2b2f5f25587c
Helena Lähdesniemi <lahdesniemihelena@gmail.com>
2022-05-10 07:23:09 +0300

    Add modifications to jetbot code

--- project_ws/src/project_pkg/project_pkg/jetbot.py
+++ project_ws/src/project_pkg/project_pkg/jetbot.py
@@ -17,58 +17,61 @@ class Jetbot(Node):
     def __init__(self):
         super().__init__('jetbot')
         self.cnt = 0
+        self.takeoff = 1
         self.id = 0
+        self.wait = 0
+        self.found_cubes = []
+        self.recent_value , self.current_value= 0,0
         self.image_sub = self.create_subscription(Image, "/Image_PubSubTopic",self.cam_callback, 10)
         self.takeoff_publisher = self.create_publisher(Empty, '/takeoff', 10)
         self.land_publisher = self.create_publisher(Empty, '/land', 10)
         self.move_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
         self.found_publisher = self.create_publisher(Int16, '/found', 10)
-        self.image_sub = self.create_subscription(Int16,'/command',self.command_callback, 10)
-
+        self.command_sub = self.create_subscription(Int16,'/command',self.command_callback, 10)
 
     def command_callback(self,msg):
-        self.id = msg
+        self.current_value = msg.data
 
     def cam_callback(self,msg):
         self.cnt += 1
-        if(self.cnt % 5 == 0):
-            cv2_img = self.imgmsg_to_cv2(msg)
-            cv.imwrite('camera_image.jpeg'.format(self.cnt), cv2_img)
+        #This is for launching the tello.It happens only once. 
+        if self.takeoff == 0:
+            msg = Empty()
+            self.takeoff_publisher.publish(msg)
+            time.sleep(2)
+            self.takeoff = 1
+        if self.current_value != self.recent_value:
+            self.id = self.current_value
+            self.wait = 0
+        self.recent_value = self.current_value
+        if self.wait == 0:
+            if(self.cnt % 2 == 0):
+                cv2_img = self.imgmsg_to_cv2(msg)
+                cv.imwrite('camera_image.jpeg'.format(self.cnt), cv2_img)
+                if self.id != 0:
+                    true = contourSquares(cv2_img, self.id)
+                    if true != 0:
+                        msg = Int16()
+                        msg.data = true
+                        self.found_publisher.publish(msg)
+                        self.wait = 1
+                        #Tää täytyy appendaa + ja varmistaa ettei sama id mee uudestaan samaan arrayhyn
+                        self.found_cubes += 1
+                    if true == 0:
+                        msg = Twist()
+                        msg.angular.z = 0.25
+                        self.move_publisher.publish(msg)
+                        time.sleep(1)
+                        msg = Twist()
+                        msg.angular.z = 0.0
+                        self.move_publisher.publish(msg)
+                        #self.wait = 1
+                    print("true on")
+                    #Muuttujaa true nimi muokataan kuvaavammaks
+                    print(true)
+                    #print(self.found_cubes)
 
-            #This is for launching the tello. This should come from the jetbot. 
-            if self.cnt == 1:
-                msg = Int16
-                self.takeoff_publisher.publish(msg)
-            #    time.sleep(2)
-            #    print("nousi")
-            self.id = 4
-            if self.id != 0:
-                true = contourSquares(cv2_img, self.id)
-                msg = Int16()
-                msg.data = true
-                self.found_publisher.publish(msg)
-                if true == 0:
-                    msg = Twist()
-                    msg.angular.z = 0.25
-                    self.move_publisher.publish(msg)
-                    time.sleep(2)
-                    msg = Twist()
-                    msg.angular.z = 0.0
-                    self.move_publisher.publish(msg)
-                    
-
-                print(true)
-                #self.found_publisher.publish(msg)
-            #cv.imshow("Result",frame)
-            #cv.waitKey(1)
-
-            #This is for landing the tello. This should be come from the jetbot. 
-            #if self.cnt == 2:
-            #    msg = Empty()
-            #    self.land_publisher.publish(msg)
-            #    print("laski")
-            #    self.cnt = 3
-            #    #value = 1
+           
 
     def imgmsg_to_cv2(self, img_msg):
         n_channels = len(img_msg.data) // (img_msg.height * img_msg.width)
